0d6a918027240e52e599176a3ff6853090d56715
Himadri <himadri.skr.das@gmail.com>
2021-09-13 18:27:09 +0530

    13th Sep .Net Core 3 with Nuget

---
+++ HelloAppCore3/HelloAppCore3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloAppCore3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(CreateMsg());
+            Console.ReadLine();
+        }
+        public static string CreateMsg()
+        {
+            return "Hello World!";
+        }
+    }
+}
