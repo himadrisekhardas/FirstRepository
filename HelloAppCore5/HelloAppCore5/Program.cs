81dd23ae4d1c7bc2396f92dd48927337b86e7787
Himadri <himadri.skr.das@gmail.com>
2021-09-13 17:52:33 +0530

    13th Sep .Net Core 5 16.11.0

---
+++ HelloAppCore5/HelloAppCore5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloAppCore5
+{
+   public class Program
+    {
+       public static void Main()
+        {
+            Console.WriteLine(CreateMsg());
+            Console.ReadLine();
+        }
+        public static string CreateMsg()
+        {
+           return "Hello World!";
+        }
+    }
+}
---
+++ HelloAppCore5/HelloAppTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using HelloAppCore5;
+
+namespace HelloAppTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World!", HelloAppCore5.Program.CreateMsg());
+        }
+    }
+}
